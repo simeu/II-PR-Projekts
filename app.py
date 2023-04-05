a6f18299b23d6628ed63cdea02c4732be707e4b8
simeu <71383052+simeu@users.noreply.github.com>
2023-04-05 11:16:26 +0300

    design element changes

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, flash
-import html
 import hashlib
 
 def savienot():
-    conn = sqlite3.connect('planotajs.db', timeout=20) #Tiek veiktw savienojums ar datubāzi, specializēti HTML.
+    conn = sqlite3.connect('planotajs.db', timeout=20) #Tiek veikts savienojums ar datubāzi, specializēti HTML.
     conn.row_factory = sqlite3.Row
     return conn
+
+def sanemt(id):
+    conn = savienot()
+    ieraksts = conn.execute("SELECT * FROM planotajs WHERE id = ?", (id,)).fetchone()
+    conn.close()
+    
+    if ieraksts is None:
+        ieraksts = {"prece": "Nezināms", "cena": "Nezināms"}
+        
+    return ieraksts
 
 app = Flask(__name__,
             template_folder="templates",
@@ -15,11 +24,19 @@ app.config["SECRET_KEY"] = "dsfn893ru9rubnfb" #Tiek noteiktas mapes, kuras tiek 
 
 @app.route('/') #Tiek nostādīta Index lapa, uz kuras balstīsies pārējie HTML faili.
 def index():
-    return render_template('index.html')
-
+    return render_template('par.html')
+'''
 @app.route("/par", methods=['GET', 'POST'])
 def par():
     return render_template('par.html')
+'''
+@app.route("/budzeta_planotajs") #Tiek parādīts plānotājs (visi ieraksti no datubāzes), kā arī "cena" tiek sasummēta, lai redzētu kopējos tēriņus. 
+def budzeta_planotajs():
+    conn = savienot()
+    planotajs = conn.execute('SELECT id, prece, cena FROM planotajs').fetchall()
+    summa = conn.execute("SELECT SUM(cena) FROM planotajs").fetchone()[0]
+    conn.close()
+    return render_template('budzeta_planotajs.html', planotajs=planotajs, summa=summa)
 
 @app.route('/jauns_planotajs', methods=['GET', 'POST']) #Tiek izveidots jauns ieraksts.
 def jauns_planotajs():
@@ -35,53 +52,9 @@ def jauns_planotajs():
     return render_template('jauns_planotajs.html')
 
 
-@app.route("/budzeta_planotajs") #Tiek parādīts plānotājs (visi ieraksti no datubāzes), kā arī "cena" tiek sasummēta, lai redzētu kopējos tēriņus. 
-def budzeta_planotajs():
-    conn = savienot()
-    planotajs = conn.execute('SELECT id, prece, cena FROM planotajs').fetchall()
-    summa = conn.execute("SELECT SUM(cena) FROM planotajs").fetchone()[0]
-    conn.close()
-    return render_template('budzeta_planotajs.html', planotajs=planotajs, summa=summa)
-
-
-
-@app.route("/registreties", methods=['GET', 'POST']) #Reģistrācija.
-def registreties():
-    conn = savienot()
-    if request.method == 'POST':
-        lietotajvards = request.form['lietotajv']
-        parole1 = request.form['parole']
-        epasts = request.form['epasts']
-        vards = request.form['vards']
-        uzvards = request.form['uzvards']
-        parole2 = hashlib.sha256(parole1.encode('utf-8')).hexdigest()
-        conn.execute("INSERT INTO lietotaji (lietotajvards, parole, epasts, vards, uzvards) VALUES (?, ?, ?, ?, ?)", (lietotajvards, parole2, epasts, vards, uzvards))
-        conn.commit()
-        conn.close()
-        return redirect(url_for('index'))
-    return render_template("registreties.html")
-
-@app.route("/ienakt", methods=['GET', 'POST']) #Pieslēgšanās.
-def ienakt():
-    conn = savienot()
-    lietotaji = conn.execute('SELECT * FROM lietotaji').fetchall()
-    conn.close()
-    if request.method == 'POST':
-        lietotajvards = request.form['lietotajv']
-        parole = request.form['parole']
-        parole = hashlib.sha256(parole.encode('utf-8')).hexdigest()
-        for lietotajs in lietotaji:
-            if lietotajvards == lietotajs['lietotajvards'] and parole == lietotajs['parole']:
-                flash("Jūs esat ienācis!")
-                return redirect(url_for('index'))
-        flash("Nepareizs lietotājvārds vai parole!")
-    return render_template('ienakt.html')
-
-@app.route("/<int:id>/labot", methods = ['GET', 'POST']) #Ieraksta labošana.
+@app.route("/<int:id>/labot", methods = ['GET', 'POST']) #Ierakstu labošana.
 def labot(id):
-    conn = savienot()
-    planotajs = conn.execute("SELECT id FROM planotajs WHERE id = ?", (id,)).fetchone()[0]
-    conn.close()
+    planotajs = sanemt(id)
     if request.method == "POST":
         prece = request.form.get('prece')
         cena = request.form.get('cena')
@@ -90,7 +63,7 @@ def labot(id):
             
         else: 
             conn = savienot()
-            conn.execute("UPDATE planotajs SET prece=? , cena=? WHERE id=?", (prece, cena, id))
+            conn.execute("UPDATE planotajs SET prece=?, cena=? WHERE id=?", (prece, cena, id))
             conn.commit()
             conn.close()
             flash("Ieraksts veiksmīgi labots!")
@@ -98,15 +71,44 @@ def labot(id):
             
     return render_template('labot.html', planotajs=planotajs)
 
-@app.route("/<int:id>/dzest", methods = ['POST']) #Ieraksta dzēšana. 
+@app.route("/<int:id>/dzest", methods = ('POST',))#Ierakstu dzēšana.
 def dzest(id):
+    planotajs = sanemt(id)
     conn = savienot()
-    conn.execute("SELECT * FROM planotajs WHERE id = ?", (id,)).fetchone()[0]
     conn.execute("DELETE FROM planotajs WHERE id = ?", (id,))
     conn.commit()
     conn.close()
-    flash("Ieraksts dzēsts.")
-    return redirect(url_for('index'))
-     
+    flash("Ieraksts veiksmīgi dzēsts!")
+    return redirect(url_for('budzeta_planotajs'))
+
+@app.route("/registreties", methods=['GET', 'POST']) #Tiek reģistrēts jauns lietotājs.
+def registreties():
+    if request.method == "POST":
+        lietotajvards = request.form.get('lietotajvards')
+        parole = request.form.get('parole')
+        parole2 = parole.sha256(request.form.get('parole').encode('utf-8')).hexdigest()
+        conn = savienot()
+        conn.execute("INSERT INTO lietotaji (lietotajvards, parole) VALUES (?, ?)", (lietotajvards, parole2))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('registreties.html')
+
+@app.route("/ienakt", methods=['GET', 'POST']) #Lietotājs pieslēdzas sistēmai.
+def ienakt():
+    if request.method == "POST":
+        lietotajvards = request.form.get('lietotajvards')
+        parole = request.form.get('parole')
+        conn = savienot()
+        lietotajs = conn.execute("SELECT * FROM lietotaji WHERE lietotajvards = ?", (lietotajvards,)).fetchone()
+        conn.close()
+        if lietotajs is None:
+            flash("Lietotājs nav atrasts! Lūdzu reģistrējieties vai pārbaudiet rakstzīmes!")
+        elif lietotajs[id] != parole:
+            flash("Nepareiza parole! Mēģiniet vēlreiz!")
+        else:
+            return redirect(url_for('budzeta_planotajs'))
+    return render_template('ienakt.html')
+         
 if __name__ == '__main__':
     app.run(debug=True)
